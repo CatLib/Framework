852fc3229bdc984a1ae51608d579b92e3d14dae7
YB <yubin@kibey.com>
2017-02-08 11:13:37 +0800

    update auto update config

--- Assets/Scripts/Config/CAutoUpdateConfig.cs
+++ Assets/Scripts/Config/CAutoUpdateConfig.cs
@@ -25,7 +25,7 @@ public class CAutoUpdateConfig : CConfig{
         {
             return new object[]
             {
-                "update.url" , new string[] { "http://pvp.oss-cn-shanghai.aliyuncs.com" }
+                "update.url" , new string[] { "http://pvp.oss-cn-shanghai.aliyuncs.com/demo" }
             };
         }
     }
