03134d26abf680dd359fb9d59bde18701fc0abc1
YB <yubin@kibey.com>
2017-02-09 18:55:25 +0800

    add cvs components interface

---
+++ Assets/Scripts/System/Lib/Contracts/CSV/ECSVLinker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CatLib.Contracts.CSV{
+
+	/// <summary>
+	/// CSV 查询连接器
+	/// </summary>
+	public enum ECSVLinker{
+
+		AND,
+
+		OR,
+
+	}
+
+}
