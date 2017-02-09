7c1ce9ca1e48a5a0c5e26fe19c11bb891679dc13
YB <yubin@kibey.com>
2017-02-09 17:17:15 +0800

    to dev level

--- Assets/Scripts/System/Lib/Support/CEnv.cs
+++ Assets/Scripts/System/Lib/Support/CEnv.cs
@@ -32,7 +32,7 @@ namespace CatLib.Support
         /// <summary>
         /// 调试等级
         /// </summary>
-        public static DebugLevels DebugLevel { get { return DebugLevels.ONLINE; } }
+        public static DebugLevels DebugLevel { get { return DebugLevels.DEV; } }
 
         /// <summary>
 		/// 编译完成后发布AssetBundle的路径
