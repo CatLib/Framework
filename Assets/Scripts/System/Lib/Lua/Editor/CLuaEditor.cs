95f4e86903a762fda5244acedfec87669d43b318
喻斌 <yb@52softs.com>
2017-02-11 10:09:23 +0800

    file clear & config xlua

---
+++ Assets/Scripts/System/Lib/Lua/Editor/CLuaEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CatLib.Lua
+{
+
+    public static class CLuaEditor
+    {
+        [CSObjectWrapEditor.GenPath]
+        public static string GenPath = Application.dataPath + "/Scripts/System/Lib/Lua/XLua/Gen/";
+    }
+
+}
---
+++ Assets/Scripts/System/Lib/Lua/XLua/Examples/08_Hotfix/HotfixTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using XLua;
+
+[Hotfix]
+public class HotfixTest : MonoBehaviour {
+    LuaEnv luaenv = new LuaEnv();
+
+    public int tick = 0; //如果是private的，在lua设置xlua.private_accessible(CS.HotfixTest)后即可访问
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if (++tick % 50 == 0)
+        {
+            Debug.Log(">>>>>>>>Update in C#, tick = " + tick);
+        }
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 100, 300, 150), "Hotfix"))
+        {
+            luaenv.DoString(@"
+                xlua.hotfix(CS.HotfixTest, 'Update', function(self)
+                    self.tick = self.tick + 1
+                    if (self.tick % 50) == 0 then
+                        print('<<<<<<<<Update in lua, tick = ' .. self.tick)
+                    end
+                end)
+            ");
+        }
+    }
+}
