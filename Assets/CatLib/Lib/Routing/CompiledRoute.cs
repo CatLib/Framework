7217461cd1a6797bdc36d467a0bce6502e2e5310
喻斌 <yb@52softs.com>
2017-03-20 21:31:56 +0800

    add license

--- Assets/CatLib/Lib/Routing/CompiledRoute.cs
+++ Assets/CatLib/Lib/Routing/CompiledRoute.cs
@@ -1,5 +1,14 @@
-﻿
-using System.Collections.Generic;
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
 using System.Text;
 
 namespace CatLib.Routing
--- Assets/CatLib/Lib/Routing/RouteCompiler.cs
+++ Assets/CatLib/Lib/Routing/RouteCompiler.cs
@@ -1,4 +1,15 @@
-﻿using System.Text.RegularExpressions;
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System.Text.RegularExpressions;
 using System.Collections;
 using CatLib.API.Routing;
 using System.Collections.Generic;
