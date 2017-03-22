c3a80532430a7def156cbdb84ba93f04c2a36e77
YB <yubin@kibey.com>
2017-03-22 14:04:40 +0800

    add lru cache component

---
+++ Assets/CatLib/Lib/LruCache/CacheNode.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+ 
+namespace CatLib.LruCache{
+
+	/// <summary>
+    /// 缓存节点
+    /// </summary>
+	public class CacheNode<TKey, TVal>{
+
+		/// <summary>
+		/// 键
+		/// </summary>
+        public TKey Key { get; private set; }
+
+		/// <summary>
+		/// 值
+		/// </summary>
+		public TVal Val { get; private set; }
+		
+		/// <summary>
+		/// 上一个节点
+		/// </summary>
+        public CacheNode<TKey, TVal> Previous { get; set; }
+        
+		/// <summary>
+		/// 下一个节点
+		/// </summary>
+		public CacheNode<TKey, TVal> Next { get; set; }
+
+		/// <summary>
+		/// 创建一个缓存节点
+		/// </summary>
+        public CacheNode(TKey key , TVal val)
+        {	
+			Key = key;
+            Val = val;
+        }
+
+	}
+
+}
