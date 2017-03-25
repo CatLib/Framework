b331be9ac0b7b1a0c39bb32e04cd59c257d347d5
喻斌 <yb@52softs.com>
2017-03-25 16:17:49 +0800

    add file license

---
+++ Assets/CatLib/Lib/Routing/Routed.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+
+namespace CatLib.Routing
+{
+
+    /// <summary>
+    /// 路由标记
+    /// </summary>
+    public class Routed : Attribute
+    {
+
+
+    }
+
+}
