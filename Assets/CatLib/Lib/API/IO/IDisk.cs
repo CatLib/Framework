9ab348168fc1aaafd893040ffd04cb3a7126d269
AlianBlank <wangfj11@foxmail.com>
2017-03-28 10:54:44 +0800

    Revert "Add annotation"
    This reverts commit c46e9566a939ac3c2ab8fca9b3780e9e0b54fe58.

--- Assets/CatLib/Lib/API/IO/IDisk.cs
+++ Assets/CatLib/Lib/API/IO/IDisk.cs
@@ -8,50 +8,24 @@
  *
  * Document: http://catlib.io/
  */
-
+ 
 using System.Collections;
 
-namespace CatLib.API.IO
-{
+namespace CatLib.API.IO{
 
-    /// <summary>
-    /// 文件驱动
-    /// </summary>
-    public interface IDisk
-    {
+	/// <summary>文件驱动</summary>
+	public interface IDisk{
 
-        /// <summary>
-        /// 获取一个文件操作对象
-        /// </summary>
-        /// <param name="path">文件路径</param>
-        /// <param name="type">路径类型 默认为相对路径</param>
-        /// <returns></returns>
-        IFile File(string path, PathTypes type = PathTypes.Relative);
+		IFile File(string path, PathTypes type = PathTypes.Relative);
 
-        /// <summary>
-        /// 获取一个目录操作对象
-        /// </summary>
-        /// <param name="path">目录路径</param>
-        /// <param name="type">路径类型 默认为相对路径</param>
-        /// <returns></returns>
-        IDirectory Directory(string path, PathTypes type = PathTypes.Relative);
+		IDirectory Directory(string path , PathTypes type = PathTypes.Relative);
 
-        /// <summary>
-        /// 文件驱动根目录
-        /// </summary>
-        IDirectory Root { get; }
+		IDirectory Root{ get; }
+		
+		bool IsCrypt{ get; }
+		
+		void SetConfig(Hashtable config);
 
-        /// <summary>
-        /// 是否加密
-        /// </summary>
-        bool IsCrypt { get; }
-
-        /// <summary>
-        /// 配置文件驱动
-        /// </summary>
-        /// <param name="config">配置表</param>
-        void SetConfig(Hashtable config);
-
-    }
+	}
 
 }
