539d289235837dcbff2783290e0883977a07d7ef
喻斌 <yb@52softs.com>
2017-03-28 20:57:50 +0800

    add priority attr

---
+++ Assets/CatLib/Lib/API/Core/Priority.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+namespace CatLib.API.Routing
+{
+    /// <summary>
+    /// 优先级标记
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+    public class Priority : Attribute
+    {
+
+        /// <summary>
+        /// 优先值
+        /// </summary>
+        public int Priorities { get; protected set; }
+
+        public Priority(int priority)
+        {
+            Priorities = priority;
+        }
+    }
+
+}
