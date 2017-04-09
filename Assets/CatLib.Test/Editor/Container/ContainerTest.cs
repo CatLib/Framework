9559ec42d7fe1e273d7a8695e2ba4fddb220243a
喻斌 <yb@52softs.com>
2017-04-09 17:52:16 +0800

    add unit test

---
+++ Assets/CatLib.Test/Editor/Container/ContainerTest.cs
@@ -0,0 +1,43 @@
+﻿
+using NUnit.Framework;
+using CatLib.API.Container;
+
+namespace CatLib.Test.Container
+{
+
+    [TestFixture]
+    public class ContainerTest
+    {
+
+        [Test]
+        public void BindFunc()
+        {
+
+            var container = MakeContainer();
+            container.Bind("Test", (cont, param) =>
+            {
+                return "HelloWorld";
+            }, true);
+
+            IBindData bind = container.GetBind("Test");
+            bool hasBind = container.HasBind("Test");
+            object obj = container.Make("Test");
+
+            Assert.AreNotEqual(null , bind);
+            Assert.AreEqual(true , hasBind);
+            Assert.AreEqual(true, bind.IsStatic);
+            Assert.AreSame("HelloWorld", obj);
+            
+
+        }
+
+
+        protected CatLib.Container.Container MakeContainer()
+        {
+            CatLib.Container.Container container = new CatLib.Container.Container();
+            return container;
+        }
+
+    }
+
+}
