42d7d763c1382a465d5b178ac9e28f9ad3dae76e
yb-nuc <yb@52softs.com>
2017-04-10 18:00:21 +0800

    config component code style optimization

--- Assets/CatLib/Lib/Config/ConfigProvider.cs
+++ Assets/CatLib/Lib/Config/ConfigProvider.cs
@@ -11,31 +11,31 @@
 using System;
 using CatLib.API.Config;
 
-namespace CatLib.Config{
+namespace CatLib.Config
+{
+    public class ConfigProvider : ServiceProvider
+    {
+        public override void Register()
+        {
+            App.Singleton<ConfigStore>().Alias<IConfigStore>().Alias("config").OnResolving((obj) =>
+            {
+                var store = obj as ConfigStore;
+                if (store == null){ return store; }
 
-	public class ConfigProvider : ServiceProvider {
+                var types = typeof(IConfig).GetChildTypesWithInterface();
 
-		public override void Register()
-		{
-
-			App.Singleton<ConfigStore>().Alias<IConfigStore>().Alias("config").OnResolving((obj)=> {
-
-                ConfigStore store = obj as ConfigStore;
-
-                Type[] types = typeof(IConfig).GetChildTypesWithInterface();
                 IConfig conf;
-                for (int i = 0; i < types.Length; i++)
+                for (var i = 0; i < types.Length; i++)
                 {
                     conf = App.Make(types[i].ToString(), null) as IConfig;
-                    store.AddConfig(conf);
+                    if (conf != null)
+                    {
+                        store.AddConfig(conf);
+                    }
                 }
 
                 return store;
-
             });
-
-		}
-
-	}
-
+        }
+    }
 }
--- Assets/CatLib/Lib/Config/ConfigStore.cs
+++ Assets/CatLib/Lib/Config/ConfigStore.cs
@@ -8,26 +8,25 @@
  *
  * Document: http://catlib.io/
  */
- 
+
 using System;
 using System.Collections.Generic;
 using CatLib.API.Config;
 using CatLib.API;
 
-namespace CatLib.Config{
-
-	public class ConfigStore : IConfigStore{
-
+namespace CatLib.Config
+{
+    public class ConfigStore : IConfigStore
+    {
         [Dependency]
         public IApplication App { get; set; }
 
-        protected Dictionary<string, Dictionary<string, object>> configs;
+        private readonly Dictionary<string, Dictionary<string, object>> configs;
 
-		public ConfigStore(){
-
-			configs = new Dictionary<string, Dictionary<string, object>>();
-
-		}
+        public ConfigStore()
+        {
+            configs = new Dictionary<string, Dictionary<string, object>>();
+        }
 
         /// <summary>
         /// 增加配置
@@ -39,79 +38,65 @@ namespace CatLib.Config{
             configs.Add(config.Name.ToString(), ParseConfig(config));
         }
 
-		public T Get<T>(Type name, string field , T def = default(T)){
+        public T Get<T>(Type name, string field, T def = default(T))
+        {
+            return Get(name.ToString(), field, def);
+        }
 
-			return Get<T>(name.ToString() , field , def);
-
-		}
-
-		public T Get<T>(string name, string field , T def = default(T))
-		{
-
-			try{
-
-				object obj = Get(name , field , (object)def);
-				if(obj == null){ return def; }
-
-				if(typeof(T) == typeof(int)){
-
-					return (T)Convert.ChangeType(obj, typeof(int));
-
-				}
-				if(typeof(T) == typeof(string)){
-
-					return (T)Convert.ChangeType(obj , typeof(string));
-
-				}
-				return (T)obj;
-
-			}catch{ throw new ArgumentException(" field [" + field + "] is can not conversion to " + typeof(T).ToString()); }
-
-		}
-
-		public string Get(Type name , string field , string def){
-
-			return Get(name.ToString(), field , def);
-
-		}
-
-		public string Get(string name , string field , string def){
-
-			return Get<string>(name , field , def);
-
-		}
-
-		public object Get(Type name , string field , object def){
-
-			return Get(name.ToString(), field , def);
-
-		}
-
-		public object Get(string name , string field , object def){
-
-			if(!configs.ContainsKey(name)){ return def; }
-			if(!configs[name].ContainsKey(field)){ return def; }
-
-			return configs[name][field];
-
-		}
-
-		protected Dictionary<string , object> ParseConfig(IConfig config){
-
-			if(config.Config.Length <= 0){ return new Dictionary<string , object>(); }
-			if (config.Config.Length % 2 != 0) { throw new ArgumentException("param is not incorrect"); }
-
-			Dictionary<string , object> fields = new Dictionary<string , object>();
-			object[] param = config.Config;
-            for (int i = 0; i < param.Length; i += 2)
+        public T Get<T>(string name, string field, T def = default(T))
+        {
+            try
             {
-                fields.Add(param[i].ToString() , param[i + 1]);
+                var obj = Get(name, field, (object)def);
+                if (obj == null) { return def; }
+
+                if (typeof(T) == typeof(int))
+                {
+                    return (T)Convert.ChangeType(obj, typeof(int));
+                }
+                if (typeof(T) == typeof(string))
+                {
+                    return (T)Convert.ChangeType(obj, typeof(string));
+                }
+                return (T)obj;
+            }
+            catch { throw new ArgumentException(" field [" + field + "] is can not conversion to " + typeof(T)); }
+        }
+
+        public string Get(Type name, string field, string def)
+        {
+            return Get(name.ToString(), field, def);
+        }
+
+        public string Get(string name, string field, string def)
+        {
+            return Get<string>(name, field, def);
+        }
+
+        public object Get(Type name, string field, object def)
+        {
+            return Get(name.ToString(), field, def);
+        }
+
+        public object Get(string name, string field, object def)
+        {
+            if (!configs.ContainsKey(name)) { return def; }
+            return !configs[name].ContainsKey(field) ? def : configs[name][field];
+        }
+
+        protected Dictionary<string, object> ParseConfig(IConfig config)
+        {
+            if (config.Config.Length <= 0) { return new Dictionary<string, object>(); }
+            if (config.Config.Length % 2 != 0) { throw new ArgumentException("param is not incorrect"); }
+
+            var fields = new Dictionary<string, object>();
+            var param = config.Config;
+            for (var i = 0; i < param.Length; i += 2)
+            {
+                fields.Add(param[i].ToString(), param[i + 1]);
             }
 
             return fields;
-
-		}
-
-	}
-
+        }
+    }
 }
