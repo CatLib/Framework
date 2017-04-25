6e904e84dc34671bac52fea8d033eeedf8b11f1d
mingtingjian <mingtingjian@sina.com>
2017-04-25 19:01:16 +0800

    Author	:	Ming ming Email	: 	mingtingjian@sina.com Date		:	2017-04-25 18:59:26 Desc	: 1、完成Lua组件的适配。 2、增加了XLua引擎接入。 3、等待单元测试和代码Review。

---
+++ Assets/Demo/Lua/Scripts/TestLuaBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using CatLib.API.Lua;
+using CatLib;
+using XLua;
+
+[LuaCallCSharp]
+public class TestLuaBehaviour : LuaMonoComponent {
+
+    public override void Init()
+    {
+        //设置代码路径
+
+        //父类方法执行Lua脚本
+        base.Init();
+    }
+}
