5c1a7e6e9d6b3e4b21ee86bb9db66b6ec846131b
mingtingjian <mingtingjian@sina.com>
2017-05-12 10:12:45 +0800

    add msgpack component (#20)
    add msgpack component

---
+++ CatLib.Unity/Assets/Demo/MsgPack/Scripts/MsgPackTestData.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Ming ming <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+using System.Collections.Generic;
+
+namespace CatLib.Demo.MsgPack
+{
+    public class MsgPackTest1Data
+    {
+        public int age;
+        public string name;
+        public bool sex;
+
+        public MsgPackTest1Data()
+        {
+            age = 1;
+            name = "test";
+            sex = false;
+        }
+    }
+
+    public class MsgPackTest2Data
+    {
+        public List<string> testList;
+        public MsgPackTest2Data()
+        {
+            testList = new List<string>();
+            testList.Add("1");
+            testList.Add("2");
+            testList.Add("3");
+            testList.Add("4");
+            testList.Add("5");
+        }
+    }
+
+    public class MsgPackTest3Data
+    {
+        public List<MsgPackTest1Data> testList;
+
+        public MsgPackTest3Data()
+        {
+            testList = new List<MsgPackTest1Data>();
+            MsgPackTest1Data data = new MsgPackTest1Data();
+            data.age = 10;
+            data.name = "CatLib";
+            data.sex = true;
+
+            testList.Add(data);
+        }
+    }
+}
+
---
+++ CatLib.Unity/Assets/Demo/MsgPack/Scripts/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Ming ming <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using CatLib.API;
+using CatLib.Event;
+using CatLib.MsgPack;
+using CatLib.Resources;
+using CatLib.IO;
+using CatLib.Time;
+using CatLib.LocalSetting;
+using CatLib;
+
+namespace CatLib.Demo.MsgPack
+{
+    /**
+     * 这个类提供了当前demo演示时用到的组件 
+     */
+    public class Bootstraps : IBootstrap
+    {
+
+        public void Bootstrap()
+        {
+            App.Instance.Register(typeof(CoreProvider));
+            App.Instance.Register(typeof(IOProvider));
+            App.Instance.Register(typeof(ResourcesProvider));
+            App.Instance.Register(typeof(TimeProvider));
+            App.Instance.Register(typeof(LocalSettingProvider));
+            App.Instance.Register(typeof(MsgPackProvider));
+            App.Instance.Register(typeof(EventProvider));
+            App.Instance.Register(typeof(MsgPackDemo));
+        }
+
+    }
+
+    public class Program : UnityEngine.MonoBehaviour {
+
+    	// Use this for initialization
+        public void Awake() {
+            (new Application(this)).Bootstrap(new Type[] { typeof(Bootstraps)}).Init();
+    	}
+
+    }
+}
