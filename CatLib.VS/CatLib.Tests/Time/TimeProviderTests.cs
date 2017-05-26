c8b215b8327cc8db0f0218df7e8f4b5eeee4e6e5
喻斌 <yb@52softs.com>
2017-05-26 20:49:36 +0800

    add unit test

--- CatLib.VS/CatLib.Tests/Time/TimeProviderTests.cs
+++ CatLib.VS/CatLib.Tests/Time/TimeProviderTests.cs
@@ -29,6 +29,94 @@ namespace CatLib.Tests.Time
     [TestClass]
     public class TimeProviderTests
     {
+        /// <summary>
+        /// 这是一个调试的时间
+        /// </summary>
+        public class TestTime : ITime
+        {
+            /// <summary>
+            /// 从游戏开始到现在所用的时间(秒)
+            /// </summary>
+            public float Time { get; private set; }
+
+            /// <summary>
+            /// 上一帧到当前帧的时间(秒)
+            /// </summary>
+            public float DeltaTime { get; private set; }
+
+            /// <summary>
+            /// 从游戏开始到现在的时间（秒）使用固定时间来更新
+            /// </summary>
+            public float FixedTime { get; private set; }
+
+            /// <summary>
+            /// 从当前scene开始到目前为止的时间(秒)
+            /// </summary>
+            public float TimeSinceLevelLoad { get; private set; }
+
+            /// <summary>
+            /// 固定的上一帧到当前帧的时间(秒)
+            /// </summary>
+            public float FixedDeltaTime { get; set; }
+
+            /// <summary>
+            /// 能获取最大的上一帧到当前帧的时间(秒)
+            /// </summary>
+            public float MaximumDeltaTime { get; private set; }
+
+            /// <summary>
+            /// 平稳的上一帧到当前帧的时间(秒)，根据前N帧的加权平均值
+            /// </summary>
+            public float SmoothDeltaTime { get; private set; }
+
+            /// <summary>
+            /// 时间缩放系数
+            /// </summary>
+            public float TimeScale { get; set; }
+
+            /// <summary>
+            /// 总帧数
+            /// </summary>
+            public float FrameCount { get; private set; }
+
+            /// <summary>
+            /// 自游戏开始后的总时间（暂停也会增加）
+            /// </summary>
+            public float RealtimeSinceStartup { get; private set; }
+
+            /// <summary>
+            /// 每秒的帧率
+            /// </summary>
+            public int CaptureFramerate { get; set; }
+
+            /// <summary>
+            /// 不考虑时间缩放上一帧到当前帧的时间(秒)
+            /// </summary>
+            public float UnscaledDeltaTime { get; private set; }
+
+            /// <summary>
+            /// 不考虑时间缩放的从游戏开始到现在的时间
+            /// </summary>
+            public float UnscaledTime { get; private set; }
+
+            public TestTime()
+            {
+                Time = 0;
+                DeltaTime = 0;
+                FixedTime = 0;
+                TimeSinceLevelLoad = 0;
+                FixedDeltaTime = 0;
+                MaximumDeltaTime = 0;
+                SmoothDeltaTime = 0;
+                TimeScale = 0;
+                FrameCount = 0;
+                RealtimeSinceStartup = 0;
+                CaptureFramerate = 0;
+                UnscaledDeltaTime = 0;
+                UnscaledTime = 0;
+            }
+        }
+
         [TestMethod]
         public void TestInitialize()
         {
@@ -38,17 +126,17 @@ namespace CatLib.Tests.Time
             app.Init();
 
             var timeManager = app.Make<ITimeManager>();
-            timeManager.Extend(() => null, "test");
+            timeManager.Extend(() => new TestTime(), "test");
 
             var config = app.Make<IConfigManager>();
             config.Default.Set("times.default", "test");
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestTimeGetDefault()
         {
             var timeManager = App.Instance.Make<ITimeManager>();
-            
+            Assert.AreEqual(typeof(TestTime), timeManager.Get().GetType());
         }
     }
 }
