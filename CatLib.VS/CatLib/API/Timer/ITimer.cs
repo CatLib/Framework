ae6f07729bd858b248614546df0199701e1548da
喻斌 <yb@52softs.com>
2017-05-28 16:49:50 +0800

    rebuild time queue component(undone)

---
+++ CatLib.VS/CatLib/API/Timer/ITimer.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+namespace CatLib.API.Timer
+{
+    /// <summary>
+    /// 计时器
+    /// </summary>
+    public interface ITimer
+    {
+
+    }
+}
