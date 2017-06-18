edc6e8ebe0129125f34c14ca52f76e8f7de5a034
yb-nuc <yb@52softs.com>
2017-06-18 18:03:00 +0800

    add unit test

---
+++ CatLib.VS/CatLib.Tests/Translation/SelectorTests.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Document: http://catlib.io/
+ */
+
+using CatLib.API.Translation;
+using CatLib.Translation;
+#if UNITY_EDITOR || NUNIT
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+#endif
+
+namespace CatLib.Tests.Translation
+{
+    [TestClass]
+    public class SelectorTests
+    {
+        [TestMethod]
+        public void TestBaseSelect()
+        {
+            var selector = new Selector();
+            Assert.AreEqual("hello this is test", selector.Choose("hello this is test", 10, Language.Chinese));
+        }
+    }
+}
