aab9dc00db8157626f0b8463fb5e3990be4ebf6c
喵喵大人 <support@catlib.io>
2017-07-02 14:04:55 +0800

    add logger interface

---
+++ CatLib.VS/CatLib/API/Debugger/IDebugger.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+namespace CatLib.API.Debugger
+{
+    /// <summary>
+    /// 调试器
+    /// 
+    /// </summary>
+    public interface IDebugger : ILogger
+    {
+        /// <summary>
+        /// 定义组
+        /// </summary>
+        /// <param name="groupName">分组名</param>
+        /// <param name="nickname">昵称(用于在调试控制器显示)</param>
+        void DefinedGroup(string groupName, string nickname);
+    }
+}
