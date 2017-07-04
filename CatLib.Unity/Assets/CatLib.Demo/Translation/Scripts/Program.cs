2d2afb00920c206f1e3ece7e09d72700003aa209
喵喵大人 <support@catlib.io>
2017-07-04 18:59:17 +0800

    add translation demo

---
+++ CatLib.Unity/Assets/CatLib.Demo/Translation/Scripts/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using CatLib.API;
+using CatLib.Core;
+using CatLib.Translation;
+
+namespace CatLib.Demo.Translation
+{
+    /**
+     * 这个类提供了当前demo演示时用到的组件 
+     */
+    public class Bootstraps : IBootstrap
+    {
+        public void Bootstrap()
+        {
+            App.Instance.Register(typeof(TranslationProvider));
+            App.Instance.Register(typeof(TranslationDemo));
+        }
+    }
+
+    /**
+     * 这个类是入口类用于启动框架 
+     */
+    public class Program : UnityEngine.MonoBehaviour
+    {
+        public void Awake()
+        {
+            var application = new Application(this);
+            application.OnFindType((type) =>
+            {
+                return Type.GetType(type);
+            });
+            application.Bootstrap(new Type[] { typeof(Bootstraps) }).Init();
+        }
+    }
+}
