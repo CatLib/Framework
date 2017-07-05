5a6e2cf3d60d3b7b2f7c51cb4f4b14956e6de307
喵喵大人 <support@catlib.io>
2017-07-05 17:49:56 +0800

    add base logger

---
+++ CatLib.VS/CatLib/Debugger/Logger.cs
@@ -0,0 +1,139 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using CatLib.API.Debugger;
+using CatLib.Stl;
+
+namespace CatLib.Debugger
+{
+    /// <summary>
+    /// 日志系统
+    /// </summary>
+    public sealed class Logger : ILogger
+    {
+        /// <summary>
+        /// 处理器
+        /// </summary>
+        private readonly SortSet<ILogHandler, int> handlers;
+
+        /// <summary>
+        /// 构造一个日志系统
+        /// </summary>
+        public Logger()
+        {
+            handlers = new SortSet<ILogHandler, int>();
+        }
+
+        /// <summary>
+        /// 增加日志处理器
+        /// </summary>
+        /// <param name="handler">处理器</param>
+        /// <param name="sort">排序</param>
+        public void AddLogHandler(ILogHandler handler , int sort = int.MaxValue)
+        {
+            Guard.Requires<ArgumentNullException>(handler != null);
+            handlers.Add(handler, sort);
+        }
+
+        /// <summary>
+        /// 输出一条日志，日志级别为传入的等级
+        /// </summary>
+        /// <param name="level">日志等级</param>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        /// <exception cref="InvalidArgumentException">当传入的日志等级无效</exception>
+        public void Log(LogLevels level, object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条调试级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Debug(object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条信息级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Info(object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条通知级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Notice(object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条警告级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Warning(object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条错误级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Error(object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条关键级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Critical(object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条警报级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Alert(object message, params object[] context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 输出一条紧急级日志
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        /// <param name="context">上下文,用于替换占位符</param>
+        public void Emergency(object message, params object[] context)
+        {
+            
+        }
+    }
+}
