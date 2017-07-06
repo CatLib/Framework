5a915a872b6a53b3454a832e4abf796d93b30f31
喵喵大人 <yb@52softs.com>
2017-07-06 09:03:15 +0800

    add sum monitor handler

---
+++ CatLib.VS/CatLib/Debugger/MonitorHandler/SumMonitorHandler.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+namespace CatLib.Debugger.MonitorHandler
+{
+    /// <summary>
+    /// 累加监控处理器
+    /// </summary>
+    public sealed  class SumMonitorHandler
+    {
+
+    }
+}
