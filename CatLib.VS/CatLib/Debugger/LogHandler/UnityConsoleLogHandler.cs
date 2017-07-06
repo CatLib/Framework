c8d64d8e007d2af189ab3e148b1d2699ab2510d3
喵喵大人 <yb@52softs.com>
2017-07-06 09:00:08 +0800

    add UnityLogHandler

--- CatLib.VS/CatLib/Debugger/LogHandler/UnityConsoleLogHandler.cs
+++ CatLib.VS/CatLib/Debugger/LogHandler/UnityConsoleLogHandler.cs
@@ -9,7 +9,10 @@
  * Document: http://catlib.io/
  */
 
+using System;
+using System.Collections.Generic;
 using CatLib.API.Debugger;
+using UnityEngine;
 
 namespace CatLib.Debugger.LogHandler
 {
@@ -19,6 +22,29 @@ namespace CatLib.Debugger.LogHandler
     class UnityConsoleLogHandler : ILogHandler
     {
         /// <summary>
+        /// 实际处理方法
+        /// </summary>
+        private Dictionary<LogLevels, Action<object>> mapping;
+
+        /// <summary>
+        /// Unity控制台日志处理器
+        /// </summary>
+        public UnityConsoleLogHandler()
+        {
+            mapping = new Dictionary<LogLevels, Action<object>>()
+            {
+                { LogLevels.Emergency , Debug.LogError },
+                { LogLevels.Alert , Debug.LogError },
+                { LogLevels.Critical , Debug.LogError },
+                { LogLevels.Error, Debug.LogError },
+                { LogLevels.Warning, Debug.LogWarning },
+                { LogLevels.Notice, Debug.Log },
+                { LogLevels.Informational, Debug.Log },
+                { LogLevels.Debug , Debug.Log }
+            };
+        }
+
+        /// <summary>
         /// 日志处理器
         /// </summary>
         /// <param name="level">日志等级</param>
@@ -26,9 +52,12 @@ namespace CatLib.Debugger.LogHandler
         /// <param name="context">上下文,用于替换占位符</param>
         public void Handler(LogLevels level, object message, params object[] context)
         {
-            
+            var result = string.Format(message.ToString(), context);
+            Action<object> handler;
+            if (mapping.TryGetValue(level, out handler))
+            {
+                handler.Invoke(result);
+            }
         }
-
-        //private void
     }
 }
