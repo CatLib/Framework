e5c92706de4bb7163218f12c7d5f456fd2e01e04
喵喵大人 <support@catlib.io>
2017-07-07 18:21:46 +0800

    add json support

---
+++ CatLib.VS/CatLib/API/Json/IJsonAware.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+namespace CatLib.API.Json
+{
+    /// <summary>
+    /// Json实例接口
+    /// </summary>
+    public interface IJsonAware
+    {
+        /// <summary>
+        /// 设定json处理器实例接口
+        /// </summary>
+        /// <param name="handler">json处理器</param>
+        void SetJson(IJson handler);
+    }
+}
---
+++ CatLib.VS/CatLib/Json/SimpleJsonAdapter.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using CatLib.API.Json;
+
+namespace CatLib.Json
+{
+    /// <summary>
+    /// Facebook Simple Json 适配器
+    /// </summary>
+    public sealed class SimpleJsonAdapter : IJson
+    {
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T">类型</typeparam>
+        /// <param name="json">json数据</param>
+        /// <returns>反序列化的类型</returns>
+        public T Decode<T>(string json)
+        {
+            return SimpleJson.SimpleJson.DeserializeObject<T>(json);
+        }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="item">需要序列化的对象</param>
+        /// <returns>json数据</returns>
+        public string Encode(object item)
+        {
+            return SimpleJson.SimpleJson.SerializeObject(item);
+        }
+    }
+}
