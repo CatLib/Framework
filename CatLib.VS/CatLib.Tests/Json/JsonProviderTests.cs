c1d14f0611330f39ce334d4c9e22a83d91ebf3f8
喵喵大人 <support@catlib.io>
2017-07-07 19:03:07 +0800

    update json

---
+++ CatLib.VS/CatLib.Tests/Json/JsonProviderTests.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+#if UNITY_EDITOR || NUNIT
+using NUnit.Framework;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+#else
+using System.Collections.Generic;
+using CatLib.API;
+using CatLib.API.Json;
+using CatLib.Core;
+using CatLib.Json;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Category = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+#endif
+
+namespace CatLib.Tests.Json
+{
+    [TestClass]
+    public class JsonProviderTests
+    {
+        /// <summary>
+        /// 准备测试环境
+        /// </summary>
+        /// <returns></returns>
+        public IApplication MakeApplication()
+        {
+            var app = new Application();
+            app.Bootstrap();
+            app.Register(new JsonProvider());
+            app.Init();
+            return app;
+        }
+
+        public class DemoClass
+        {
+            public string Name;
+
+            public Dictionary<string, Dictionary<string, DemoSubClass>> Dict;
+        }
+
+        public class DemoSubClass
+        {
+            public int Age;
+        }
+
+        [TestMethod]
+        public void TestJsonEncodeDecode()
+        {
+            var app = MakeApplication();
+            var json = app.Make<IJson>();
+            var demoClass = new DemoClass()
+            {
+                Name = "helloworld",
+                Dict = new Dictionary<string, Dictionary<string, DemoSubClass>>()
+                {
+                    {"key" , new Dictionary<string, DemoSubClass>()
+                        {
+                            { "key" , new DemoSubClass(){ Age = 18 } }
+                        }
+                    }
+                }
+            };
+
+            var jsonStr = json.Encode(demoClass);
+            var decodeClass = json.Decode<DemoClass>(jsonStr);
+
+            Assert.AreEqual("helloworld", decodeClass.Name);
+            Assert.AreEqual(18, decodeClass.Dict["key"]["key"].Age);
+        }
+    }
+}
