9ccc88f692de7393f16640f1bbf38fd9faab0789
喵喵大人 <yb@52softs.com>
2017-07-12 07:48:39 +0800

    add monitor content

---
+++ CatLib.VS/CatLib/Debugger/WebMonitor/Handler/CallbackMonitorHandler.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using CatLib.Stl;
+
+namespace CatLib.Debugger.WebMonitor.Handler
+{
+    /// <summary>
+    /// 回调获取监控值处理器
+    /// </summary>
+    public sealed class CallbackMonitorHandler : IMonitorHandler
+    {
+        /// <summary>
+        /// 监控的标题
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// 监控值的单位描述
+        /// </summary>
+        public string Unit { get; private set; }
+
+        /// <summary>
+        /// 实时的监控值
+        /// </summary>
+        public string Value
+        {
+            get { return callback.Invoke().ToString(); }
+        }
+
+        /// <summary>
+        /// 回调
+        /// </summary>
+        private Func<object> callback;
+
+        /// <summary>
+        /// 单次记录监控处理器
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="unit">单位值</param>
+        /// <param name="callback">回调获取监控值</param>
+        public CallbackMonitorHandler(string title, string unit, Func<object> callback)
+        {
+            Guard.Requires<ArgumentNullException>(callback != null);
+            Title = title;
+            Unit = unit;
+            this.callback = callback;
+        }
+
+        /// <summary>
+        /// 处理句柄
+        /// </summary>
+        /// <param name="value">值</param>
+        public void Handler(object value)
+        {
+        }
+    }
+}
---
+++ CatLib.VS/CatLib/Debugger/WebMonitorContent/HeapMemory.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using CatLib.Debugger.WebMonitor;
+using CatLib.Debugger.WebMonitor.Handler;
+
+namespace CatLib.Debugger.WebMonitorContent
+{
+    /// <summary>
+    /// 堆内存
+    /// </summary>
+    public sealed class HeapMemory
+    {
+        /// <summary>
+        /// 构建一个堆内存监控
+        /// </summary>
+        /// <param name="monitor">监控</param>
+        public HeapMemory([Inject(Required = true)]IMonitor monitor)
+        {
+            //monitor.DefinedMoitor("heap.memory", );
+        }
+    }
+}
