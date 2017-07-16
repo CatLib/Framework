2fac9d76a6dc1c2447651b8fe69ca4320259d464
喵喵大人 <support@catlib.io>
2017-07-16 12:18:16 +0800

    add converters

---
+++ CatLib.VS/CatLib/Converters/Plan/StringInt16Converter.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using CatLib.API.Converters;
+
+namespace CatLib.Converters.Plan
+{
+    /// <summary>
+    /// string转int16转换器
+    /// </summary>
+    public sealed class StringInt16Converter : ITypeConverter
+    {
+        /// <summary>
+        /// 来源
+        /// </summary>
+        public Type From
+        {
+            get
+            {
+                return typeof(string);
+            }
+        }
+
+        /// <summary>
+        /// 目标
+        /// </summary>
+        public Type To
+        {
+            get
+            {
+                return typeof(short);
+            }
+        }
+
+        /// <summary>
+        /// 源类型转换到目标类型
+        /// </summary>
+        /// <param name="source">源类型</param>
+        /// <param name="to">目标类型</param>
+        /// <returns>目标类型</returns>
+        public object ConvertTo(object source, Type to)
+        {
+            return short.Parse(source.ToString());
+        }
+    }
+}
