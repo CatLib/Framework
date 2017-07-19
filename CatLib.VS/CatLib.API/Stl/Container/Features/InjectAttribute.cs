33a702e291c95ee8ece40f380c0bfbf1307fc690
喵喵大人 <support@catlib.io>
2017-07-19 15:45:00 +0800

    interface update

--- CatLib.VS/CatLib.API/Stl/Container/Features/InjectAttribute.cs
+++ CatLib.VS/CatLib.API/Stl/Container/Features/InjectAttribute.cs
@@ -11,36 +11,39 @@
 
 using System;
 
-/// <summary>
-/// 注入标记
-/// </summary>
-[AttributeUsage(AttributeTargets.Interface | 
-                AttributeTargets.Parameter |
-                AttributeTargets.Property)]
-public sealed class InjectAttribute : Attribute
+namespace CatLib
 {
     /// <summary>
-    /// 注入服务的别名或者服务名
+    /// 注入标记
     /// </summary>
-    public string Alias { get; private set; }
-
-    /// <summary>
-    /// 是否是必须的
-    /// 如果约束为必须当依赖注入失败时则会引发一个异常
-    /// </summary>
-    public bool Required { get; set; }
-
-    /// <summary>
-    /// 声明注入
-    /// </summary>
-    /// <param name="alias">依赖服务的别名或者服务名</param>
-    public InjectAttribute(string alias)
+    [AttributeUsage(AttributeTargets.Interface |
+                    AttributeTargets.Parameter |
+                    AttributeTargets.Property)]
+    public sealed class InjectAttribute : Attribute
     {
-        Alias = alias;
-    }
+        /// <summary>
+        /// 注入服务的别名或者服务名
+        /// </summary>
+        public string Alias { get; private set; }
 
-    /// <summary>
-    /// 声明注入
-    /// </summary>
-    public InjectAttribute() { }
+        /// <summary>
+        /// 是否是必须的
+        /// 如果约束为必须当依赖注入失败时则会引发一个异常
+        /// </summary>
+        public bool Required { get; set; }
+
+        /// <summary>
+        /// 声明注入
+        /// </summary>
+        /// <param name="alias">依赖服务的别名或者服务名</param>
+        public InjectAttribute(string alias)
+        {
+            Alias = alias;
+        }
+
+        /// <summary>
+        /// 声明注入
+        /// </summary>
+        public InjectAttribute() { }
+    }
 }
