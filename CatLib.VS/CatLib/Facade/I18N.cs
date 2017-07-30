b1ae09c28017d9ef437ba8119b3372aa93dbb3bc
喵喵大人 <support@catlib.io>
2017-07-30 19:52:04 +0800

    add i18n facade

---
+++ CatLib.VS/CatLib/Facade/I18N.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using CatLib.API.Translation;
+
+namespace CatLib.Facade
+{
+    /// <summary>
+    /// 国际化I18N
+    /// </summary>
+    public sealed class I18N : Facade<ITranslator>
+    {
+    }
+}
