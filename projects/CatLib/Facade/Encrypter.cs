d60ebb9ee3d68616456aa0f42bbeb2b864b75c4c
喵喵大人 <support@catlib.io>
2017-09-08 14:19:11 +0800

    add facade

---
+++ projects/CatLib/Facade/Encrypter.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using CatLib.API.Encryption;
+
+namespace CatLib.Facade
+{
+    /// <summary>
+    /// 加密器
+    /// </summary>
+    public sealed class Encrypter : Facade<IEncrypter>
+    {
+    }
+}
