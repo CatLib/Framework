8abd8ad5fc56c404fe221746d50006a448d06491
喵喵大人 <support@catlib.io>
2017-09-25 14:38:33 +0800

    file structure adjustment

---
+++ projects/CatLib.Framework.API/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CatLib.API")]
+[assembly: AssemblyDescription("CatLib Component-Based Framework API")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CatLib")]
+[assembly: AssemblyProduct("CatLib.Framework.API")]
+[assembly: AssemblyCopyright("Copyright © CatLib 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5bf5d9dc-26f3-46ae-a735-fe37b0d4fe2e")]
+
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
---
+++ projects/CatLib.Framework/Routing/RoutingProvider.cs
@@ -0,0 +1,114 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+#if CATLIB
+using System;
+using CatLib.API.Routing;
+using System.Collections.Generic;
+
+namespace CatLib.Routing
+{
+    /// <summary>
+    /// 路由服务
+    /// </summary>
+    public sealed class RoutingProvider : IServiceProvider
+    {
+        /// <summary>
+        /// 默认的Scheme
+        /// </summary>
+        [Config]
+        public string DefaultScheme { get; set; }
+
+        /// <summary>
+        /// 是否进行路由编译
+        /// </summary>
+        [Config]
+        public bool RouterCompiler { get; set; }
+
+        /// <summary>
+        /// 会进行属性路由编译的程序集
+        /// </summary>
+        [Config]
+        public IList<string> CompilerAssembly { get; set; }
+
+        /// <summary>
+        /// 路由服务
+        /// </summary>
+        public RoutingProvider()
+        {
+            DefaultScheme = "catlib";
+            RouterCompiler = true;
+        }
+
+        /// <summary>
+        /// 执行路由编译，路由编译总是最后进行的
+        /// </summary>
+        /// <returns>迭代器</returns>
+        public void Init()
+        {
+            if (!RouterCompiler)
+            {
+                return;
+            }
+            var router = App.Make<Router>();
+            router.RouterCompiler();
+        }
+
+        /// <summary>
+        /// 注册路由条目
+        /// </summary>
+        public void Register()
+        {
+            App.Singleton<Router>((_, __) =>
+            {
+                var router = new Router(App.Handler, App.Handler);
+                router.SetDefaultScheme(DefaultScheme);
+                return router;
+            }).Alias<IRouter>();
+
+            RegisterAttrRouteCompiler();
+            RegisterFinderType();
+        }
+
+        /// <summary>
+        /// 注册获取类型
+        /// </summary>
+        private void RegisterFinderType()
+        {
+            App.OnFindType((str) => { return Type.GetType(str); }, 100);
+        }
+
+        /// <summary>
+        /// 注册属性路由编译器
+        /// </summary>
+        private void RegisterAttrRouteCompiler()
+        {
+            App.Bind<AttrRouteCompiler>().OnResolving((_, obj) =>
+            {
+                var compiler = (AttrRouteCompiler)obj;
+
+                var containList = new List<string>(CompilerAssembly ?? new List<string>())
+                {
+                    "Assembly-CSharp",
+                    "Assembly-CSharp-Editor-firstpass",
+                    "Assembly-CSharp-Editor",
+                    "CatLib.Framework",
+                    "CatLib.Framework.Tests"
+                };
+
+                compiler.OnStripping(assembly => !containList.Contains(assembly.GetName().Name));
+
+                return obj;
+            });
+        }
+    }
+}
+#endif
