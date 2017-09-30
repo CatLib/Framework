58bb70ef4918524d76803e7dd289ff8047fcf524
喵喵大人 <support@catlib.io>
2017-09-30 15:27:05 +0800

    add packer & state

---
+++ src/CatLib.Framework/Network/HeartBeatState.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+
+namespace CatLib.Network
+{
+    /// <summary>
+    /// 心跳包状态
+    /// </summary>
+    internal sealed class HeartBeatState
+    {
+        /// <summary>
+        /// 流逝时间
+        /// </summary>
+        private int elapseSeconds;
+
+        /// <summary>
+        /// 触发间隔
+        /// </summary>
+        private int interval;
+
+        /// <summary>
+        /// 心跳丢失次数
+        /// </summary>
+        private int missCount;
+
+        /// <summary>
+        /// 当丢失心跳包
+        /// </summary>
+        public event Action<int> OnMissHeartBeat;
+
+        /// <summary>
+        /// 心跳包状态
+        /// </summary>
+        public HeartBeatState(int interval)
+        {
+            this.interval = interval;
+            Reset();
+        }
+
+        /// <summary>
+        /// 设定心跳包间隔
+        /// </summary>
+        /// <param name="interval">间隔</param>
+        public void SetInterval(int interval)
+        {
+            this.interval = interval;
+        }
+
+        /// <summary>
+        /// 触发心跳流逝
+        /// </summary>
+        /// <param name="elapseSeconds">流逝时间</param>
+        public void Tick(int elapseSeconds)
+        {
+            if (interval <= 0)
+            {
+                return;
+            }
+
+            this.elapseSeconds += Math.Max(elapseSeconds, 0);
+
+            if (!(this.elapseSeconds >= interval))
+            {
+                return;
+            }
+
+            missCount++;
+            this.elapseSeconds = 0;
+            if (OnMissHeartBeat != null)
+            {
+                OnMissHeartBeat.Invoke(missCount);
+            }
+        }
+
+        /// <summary>
+        /// 重置心跳包状态
+        /// </summary>
+        public void Reset()
+        {
+            elapseSeconds = 0;
+            missCount = 0;
+        }
+    }
+}
---
+++ src/CatLib.Framework/Network/ReceiveState.cs
@@ -0,0 +1,117 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using System.IO;
+using CatLib.API.Network;
+
+namespace CatLib.Network
+{
+    /// <summary>
+    /// 接收状态
+    /// </summary>
+    internal sealed class ReceiveState
+    {
+        /// <summary>
+        /// 数据流
+        /// </summary>
+        private readonly MemoryStream stream;
+
+        /// <summary>
+        /// 包长
+        /// </summary>
+        private int packetLength;
+
+        /// <summary>
+        /// 解包器
+        /// </summary>
+        private readonly IPacker packer;
+
+        /// <summary>
+        /// 接收状态
+        /// </summary>
+        public ReceiveState(IPacker packer)
+        {
+            stream = new MemoryStream(1024 * 64);
+            packetLength = 0;
+            this.packer = packer;
+        }
+
+        /// <summary>
+        /// 写入数据，通过返回值可以判断是否符合解包条件
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="ex">异常</param>
+        /// <returns>解包后的数据，如果返回null则代表没有解包</returns>
+        public object Input(byte[] data, out Exception ex)
+        {
+            ex = null;
+
+            if (stream.Position != stream.Length)
+            {
+                stream.Seek(stream.Length, SeekOrigin.Begin);
+            }
+
+            stream.Write(data, 0, data.Length);
+
+            if (packetLength <= 0)
+            {
+                var receiveData = new byte[stream.Length];
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.Read(receiveData, 0, receiveData.Length);
+                packetLength = Math.Max(packer.Input(receiveData, out ex), 0);
+                if (ex != null)
+                {
+                    return null;
+                }
+            }
+
+            return packetLength < stream.Length ? null : Decode(out ex);
+        }
+
+        /// <summary>
+        /// 重置接收状态
+        /// </summary>
+        public void Reset()
+        {
+            stream.SetLength(0);
+            packetLength = 0;
+        }
+
+        /// <summary>
+        /// 尝试进行解包
+        /// </summary>
+        /// <param name="ex">解包发生的异常</param>
+        /// <returns>解包的数据</returns>
+        private object Decode(out Exception ex)
+        {
+            var data = new byte[packetLength];
+            stream.Seek(0, SeekOrigin.Begin);
+            var read = stream.Read(data, 0, data.Length);
+
+            if (read < data.Length)
+            {
+                var migrateData = new byte[data.Length - read];
+                stream.Read(migrateData, 0, migrateData.Length);
+                Reset();
+                stream.Write(migrateData, 0, migrateData.Length);
+            }
+            else
+            {
+                Reset();
+            }
+
+            var packet = packer.Decode(data, out ex);
+
+            return ex != null ? null : packet;
+        }
+    }
+}
