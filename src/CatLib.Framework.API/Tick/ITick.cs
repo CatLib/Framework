d985467007ff1e5001c47fd8ee84e72dc30c8769
喵喵大人 <support@catlib.io>
2017-09-30 11:33:23 +0800

    add tick

---
+++ src/CatLib.Framework.API/Tick/ITick.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+namespace CatLib.API.Tick
+{
+    /// <summary>
+    /// 滴答
+    /// </summary>
+    public interface ITick
+    {
+        /// <summary>
+        /// 滴答间流逝的时间
+        /// </summary>
+        /// <param name="elapseSeconds">完成最后一帧所花费的时间（秒）</param>
+        void Tick(float elapseSeconds);
+    }
+}
