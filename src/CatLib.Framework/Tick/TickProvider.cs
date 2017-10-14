b82201a80e52e96d66b0184985e2b13db0089e2a
喵喵大人 <support@catlib.io>
2017-10-14 17:15:27 +0800

    provider update

--- src/CatLib.Framework/Tick/TickProvider.cs
+++ src/CatLib.Framework/Tick/TickProvider.cs
@@ -14,7 +14,23 @@ namespace CatLib.Tick
     /// <summary>
     /// 时间摆钟服务提供者
     /// </summary>
-    public sealed class TickProvider
+    public sealed class TickProvider : IServiceProvider
     {
+        /// <summary>
+        /// 初始化服务提供者
+        /// </summary>
+        [Priority(5)]
+        public void Init()
+        {
+            App.Make<TimeTicker>();
+        }
+
+        /// <summary>
+        /// 注册服务提供者
+        /// </summary>
+        public void Register()
+        {
+            App.Singleton<TimeTicker>();
+        }
     }
 }
