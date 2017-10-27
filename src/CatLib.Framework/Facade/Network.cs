bc00f75157def589f5b96cbe7977b74165b0cf30
喵喵大人 <support@catlib.io>
2017-10-27 08:44:58 +0800

    add facade

---
+++ src/CatLib.Framework/Facade/Network.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using CatLib.API.Network;
+
+namespace CatLib.Facade
+{
+    /// <summary>
+    /// 网络
+    /// </summary>
+    public sealed class Network : Facade<INetworkManager>
+    {
+    }
+}
